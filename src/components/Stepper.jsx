938006c73d2a7dca66db387db7358c88975c414d
Mohan1543 <villurishankar1543@gmail.com>
2023-10-11 16:31:55 +0530

    updated stepper

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -18,7 +18,6 @@ const steps = ["Personal Information", "Flight Details", "Confirmation"];
 
 export default function HorizontalLinearStepper({ setConfirmationPage }) {
   const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
   const [formData, setFormData] = React.useState({
     firstName: "",
     lastName: "",
@@ -39,12 +38,7 @@ export default function HorizontalLinearStepper({ setConfirmationPage }) {
     return step === 1;
   };
 
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
   const handleNext = () => {
-    let newSkipped = skipped;
     if (activeStep === 0) {
       if (
         formData.firstName &&
@@ -52,13 +46,7 @@ export default function HorizontalLinearStepper({ setConfirmationPage }) {
         formData.email &&
         formData.phoneNumber
       ) {
-        if (isStepSkipped(activeStep)) {
-          newSkipped = new Set(newSkipped.values());
-          newSkipped.delete(activeStep);
-        }
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
-      } else {
         submitHandler();
       }
     }
@@ -109,17 +97,6 @@ export default function HorizontalLinearStepper({ setConfirmationPage }) {
   const handleReset = () => {
     setActiveStep(0);
   };
-  // const handleSkip = () => {
-  //   if (!isStepOptional(activeStep)) {
-  //     throw new Error("You can't skip a step that isn't optional.");
-  //   }
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setSkipped((prevSkipped) => {
-  //     const newSkipped = new Set(prevSkipped.values());
-  //     newSkipped.add(activeStep);
-  //     return newSkipped;
-  //   });
-  // };
 
   const changeHandler = (e) => {
     setFormData({
@@ -173,16 +150,8 @@ export default function HorizontalLinearStepper({ setConfirmationPage }) {
     if (hasErrors) {
       setFlightDetailsErrors(newErrors);
     } else {
-      // Submit the form data or perform other actions here
       console.log("Form submitted:", flightData);
-      let newSkipped = skipped;
-
-      if (isStepSkipped(activeStep)) {
-        newSkipped = new Set(newSkipped.values());
-        newSkipped.delete(activeStep);
-      }
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
-      setSkipped(newSkipped);
     }
   };
 
@@ -198,9 +167,6 @@ export default function HorizontalLinearStepper({ setConfirmationPage }) {
           const labelProps = {};
           if (isStepOptional(index)) {
             labelProps.optional = <Typography variant="caption" />;
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
           }
           return (
             <Step key={label} {...stepProps}>
@@ -280,11 +246,6 @@ export default function HorizontalLinearStepper({ setConfirmationPage }) {
                 </Button>
               )}
               <Box sx={{ flex: "1 1 auto" }} />
-              {/* {isStepOptional(activeStep) && (
-                <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                  Skip
-                </Button>
-              )} */}
               {activeStep !== 2 && (
                 <Button onClick={handleNext}>
                   {activeStep === steps.length - 1 ? "Finish" : "Next"}
